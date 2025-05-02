b086ad70b4d810ca6b7d83291ac793a8580cc2e5
Sebastian Weigmann <seb.weigmann@gmail.com>
2025-05-02 11:02:26 +0200

    Refactor: improved tests

--- test/test_lib.py
+++ test/test_lib.py
@@ -2,29 +2,31 @@
 # flake8: noqa: E501
 import pytest      # noqa: F401
 import subprocess
+import os
 
 
-class TestProgram(object):
+class Test_danalyze(object):
     # end-to-end tests
     def test_process(self):
         p = subprocess.run(
-            ["python3", "src/danalyze/danalyze.py", "--help"],
+            ["python3", os.path.join("src", "danalyze", "danalyze.py"), "--help"],
             capture_output=True,
             text=True,
             check=True,
             timeout=5,
         )
-        assert "usage: danalyze [-h] " in p.stdout
         assert p.returncode == 0
+        assert "usage: danalyze [-h] " in p.stdout
 
-    def test_file_ssdeep(self):
+    def test_danalyze_file_ssdeep(self):
         p = subprocess.run(
-            ["python3", "src/danalyze/danalyze.py", "test/1", "test/2"],
+            ["python3", os.path.join("src", "danalyze", "danalyze.py"), os.path.join("test", "1"), os.path.join("test", "2")],
             capture_output=True,
             text=True,
             check=True,
             timeout=5,
         )
+        assert p.returncode == 0
         assert "filepath" in p.stdout
         assert "file1_ssdeep" in p.stdout
         assert "file2_ssdeep" in p.stdout
@@ -34,23 +36,23 @@ class TestProgram(object):
             "3:AXGBicFlgVNhBGcL6wCrFQE3:AXGHsNhxLsr2s,3:AXGBicFlIHBGcL6wCrFQE3:AXGH6xLsr2s,22"
             in p.stdout
         )
-        assert p.returncode == 0
 
-    def test_file_ssdeeper(self):
+    def test_danalyze_file_ssdeeper(self):
         p = subprocess.run(
             [
                 "python3",
-                "src/danalyze/danalyze.py",
+                os.path.join("src", "danalyze", "danalyze.py"),
                 "-s",
                 "ssdeeper",
-                "test/1",
-                "test/2",
+                os.path.join("test", "1"),
+                os.path.join("test", "2"),
             ],
             capture_output=True,
             text=True,
             check=True,
             timeout=5,
         )
+        assert p.returncode == 0
         assert "filepath" in p.stdout
         assert "file1_ssdeeper" in p.stdout
         assert "file2_ssdeeper" in p.stdout
@@ -60,4 +62,3 @@ class TestProgram(object):
             "3:AN8gu5QklJgVNhyEgcGwFEBQJab:VgDhxFkb,3:AN8gu5QklJuXgcGwFEBQJab:VglxFkb,28"
             in p.stdout
         )
-        assert p.returncode == 0
